ef52236409ff98214082819bbac758f4cedb383c
YEZI KIM <yvely225@gmail.com>
2022-06-17 20:02:39 +0900

    Delete spring0617/src/main/java/com/example/spring0607 directory

--- spring0617/src/main/java/com/example/spring0607/config/jwt/InputNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.example.spring0607.config.jwt;
-
-public class InputNotFoundException extends RuntimeException{
-    public InputNotFoundException() {
-        super();
-    }
-}
--- spring0617/src/main/java/com/example/spring0607/dto/MemberRequest.java
+++
@@ -1,9 +0,0 @@
-package com.example.spring0607.dto;
-
-import lombok.Getter;
-
-@Getter
-public class MemberRequest {
-    private String userId;
-    private String userPw;
-}
